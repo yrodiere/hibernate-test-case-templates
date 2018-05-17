95358abd269eb9eac2802d4007bbefe6ca000c6b
nikowitt <github@wittenbeck-it.de>
2018-05-17 07:10:50 +0200

    Preparing for feature branches per bug

--- orm/hibernate-orm-5/src/main/java/com/enhancement/generics/Mail.java
+++
@@ -1,5 +0,0 @@
-package com.enhancement.generics;
-
-//@Entity uncomment this to crash bytecode enhancement: HHH-12421
-public class Mail extends AbstractMail {
-}
--- orm/hibernate-orm-5/src/main/java/com/enhancement/generics/Marker.java
+++
@@ -1,4 +0,0 @@
-package com.enhancement.generics;
-
-public interface Marker {
-}
