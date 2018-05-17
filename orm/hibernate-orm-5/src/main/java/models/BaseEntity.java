87cca9d29d503dde227c2a3145f0da6d16e8302f
nikowitt <github@wittenbeck-it.de>
2018-05-17 07:29:38 +0200

    Preparing for feature branches per bug

---
+++ orm/hibernate-orm-5/src/main/java/models/BaseEntity.java
@@ -0,0 +1,28 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
+
+@Entity
+@Polymorphism(type = PolymorphismType.EXPLICIT)
+@Inheritance(strategy = InheritanceType.JOINED)
+public class BaseEntity extends DatabaseEntity {
+
+	private int id;
+	
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ orm/hibernate-orm-5/src/main/java/models/DatabaseEntity.java
@@ -0,0 +1,7 @@
+package models;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class DatabaseEntity {
+}
