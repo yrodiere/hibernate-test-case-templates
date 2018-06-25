05888d4225d7a27cae04dcde7fef68609d8cbf72
Yoann Rodière <yoann@hibernate.org>
2018-06-25 13:06:43 +0200

    HHH-12594 Simplify the test case

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/TestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/TestCase.java
@@ -1,12 +1,17 @@
 package org.hibernate.bugs;
 
-import org.hibernate.cfg.AvailableSettings;
-import org.junit.Test;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.MappedSuperclass;
 
-import models.common.File;
-import models.common.security.Authority;
-import models.common.security.JafSid;
-import models.common.security.UserGroup;
+import org.hibernate.cfg.AvailableSettings;
+
+import org.junit.Test;
 
 public class TestCase extends AbstractTestCase {
 	public TestCase() {
@@ -17,6 +22,52 @@ public class TestCase extends AbstractTestCase {
 	@Test
 	public void hhh12594() {
 		System.out.println("Crashes during bootstrapping.");
+	}
+
+	@MappedSuperclass
+	public abstract static class DatabaseEntity {
+		private int id;
+
+		@Id
+		@GeneratedValue
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+	}
+
+	@Entity
+	public static class UserGroup extends DatabaseEntity {
+
+		private Set<JafSid> members = new LinkedHashSet<>();
+
+		@ManyToMany
+		public Set<JafSid> getMembers() {
+			return members;
+		}
+
+		public void setMembers(Set<JafSid> members) {
+			this.members = members;
+		}
+	}
+
+	@Entity
+	public static class JafSid extends DatabaseEntity {
+
+		private Set<UserGroup> groups = new LinkedHashSet<>();
+
+		@ManyToMany(mappedBy = "members", fetch = FetchType.EAGER)
+		public Set<UserGroup> getGroups() {
+			return groups;
+		}
+
+		public void setGroups(Set<UserGroup> groups) {
+			this.groups = groups;
+		}
 	}
 
 }
