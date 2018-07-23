e1e03d7bacbdc95dad87b9095f55501a53071e7a
Yoann Rodière <yoann@hibernate.org>
2018-07-23 14:36:18 +0200

    Update the test case with the latest information

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ImprovedPhysicalNamingStrategyImpl.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ImprovedPhysicalNamingStrategyImpl.java
@@ -1,42 +1,53 @@
-/*
- * Hibernate Search, full-text search for your domain model
- *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later
- * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
- */
 package org.hibernate.bugs;
 
-import java.io.Serializable;
 import java.util.Locale;
 
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.hibernate.internal.util.StringHelper;
 
-public class ImprovedPhysicalNamingStrategyImpl extends PhysicalNamingStrategyStandardImpl implements Serializable {
+/**
+ * @see <A HREF="https://stackoverflow.com/questions/32437202/improvednamingstrategy-no-longer-working-in-hibernate-5">
+ * ImprovedNamingStrategy no longer working in Hibernate 5</A>
+ */
+public class ImprovedPhysicalNamingStrategyImpl extends PhysicalNamingStrategyStandardImpl {
+	private static final long serialVersionUID = -5520697659246884475L;
+
+	public ImprovedPhysicalNamingStrategyImpl() {
+		super();
+	}
 
 	@Override
 	public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
-		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+		return addUnderscores(name);
 	}
 
 	@Override
 	public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment context) {
-		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+		return addUnderscores(name);
 	}
 
+	public static Identifier addUnderscores(Identifier name) {
+		String normal = normalize(name.getText());
+		return new Identifier(normal, name.isQuoted());
+	}
 
-	protected static String addUnderscores(String name) {
-		final StringBuilder buf = new StringBuilder( name.replace('.', '_') );
-		for (int i=1; i<buf.length()-1; i++) {
-			if (
-					Character.isLowerCase( buf.charAt(i-1) ) &&
-							Character.isUpperCase( buf.charAt(i) ) &&
-							Character.isLowerCase( buf.charAt(i+1) )
-					) {
+	public static String normalize(String name) {
+		String pureName = StringHelper.unqualify(name);
+		return addUnderscores(pureName);
+	}
+
+	public static String addUnderscores(String name) {
+		StringBuilder buf = new StringBuilder(name.replace('.', '_'));
+		for (int i = 1; i < buf.length() - 1; i++) {
+			if (Character.isLowerCase(buf.charAt(i - 1))
+					&& Character.isUpperCase(buf.charAt(i))
+					&& Character.isLowerCase(buf.charAt(i + 1))) {
 				buf.insert(i++, '_');
 			}
 		}
+
 		return buf.toString().toLowerCase(Locale.ROOT);
 	}
 }
