01240b544bbfd82c6836ebc87213e30b72f68fac
Bobby Philip <bobbyphilip@gmail.com>
2020-07-08 07:55:07 +0200

    Added metadata

--- search/hibernate-search-6/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/YourIT.java
+++ search/hibernate-search-6/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/YourIT.java
@@ -22,11 +22,34 @@ public class YourIT extends SearchTestBase {
 	public void testYourBug() {
 		try ( Session s = getSessionFactory().openSession() ) {
 			Product product1 = new Product( 1L, "Jane Smith" );
+			
+
 			Product product2 = new Product( 2L, "John Doe" );
 	
 			Transaction tx = s.beginTransaction();
 			s.persist( product1 );
+			ProductMetadata pm11 = new ProductMetadata();
+			pm11.setKey("genre");
+			pm11.setValue("crime");
+			pm11.setProduct(product1);
+			
+			ProductMetadata pm12 = new ProductMetadata();
+			pm12.setKey("author");
+			pm12.setValue("Christie");
+			pm12.setProduct(product1);
+			
+			
+			
 			s.persist( product2 );
+			ProductMetadata pm21 = new ProductMetadata();
+			pm21.setKey("genre");
+			pm21.setValue("spy");
+			pm21.setProduct(product2);
+			
+			ProductMetadata pm22 = new ProductMetadata();
+			pm22.setKey("author");
+			pm22.setValue("Forsyth");
+			pm22.setProduct(product2);
 			tx.commit();
 		}
 
@@ -34,7 +57,7 @@ public class YourIT extends SearchTestBase {
 			SearchSession searchSession = Search.session( session );
 
 			List<Product> hits = searchSession.search( Product.class )
-					.where( f -> f.match().field( "name" ).matching( "smith" ) )
+					.where( f -> f.match().field( "product.productmetadata" ).matching( "spy" ) )
 					.fetchHits( 20 );
 
 			assertEquals( 1, hits.size() );
