d7d9d6c01929c915ada8e896c98835e7a1ebad1d
Yoann Rodière <yoann@hibernate.org>
2021-03-10 08:24:20 +0100

    Add Lucene dependency
    To switch more easily between ES and Lucene
    
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

--- search/hibernate-search-6/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/AnalysisConfigurer.java
+++ search/hibernate-search-6/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/AnalysisConfigurer.java
@@ -2,10 +2,21 @@ package org.hibernate.search.bugs;
 
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
 
-public class AnalysisConfigurer implements ElasticsearchAnalysisConfigurer {
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
+
+public class AnalysisConfigurer implements ElasticsearchAnalysisConfigurer, LuceneAnalysisConfigurer {
 	@Override
 	public void configure(ElasticsearchAnalysisConfigurationContext context) {
-		context.analyzer("customAnalyzer").custom().tokenizer("whitespace").tokenFilters("lowercase");
+		context.analyzer( "customAnalyzer" ).custom().tokenizer( "whitespace" ).tokenFilters( "lowercase" );
+	}
+
+	@Override
+	public void configure(LuceneAnalysisConfigurationContext context) {
+		context.analyzer( "customAnalyzer" ).custom().tokenizer( WhitespaceTokenizerFactory.class )
+				.tokenFilter( LowerCaseFilterFactory.class );
 	}
 }
