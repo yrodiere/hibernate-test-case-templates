8e92db7c3f0c5151d2159e9062b492858cac49f4
Yoann Rodière <yoann@hibernate.org>
2022-02-24 12:04:44 +0100

    Reproducer

--- orm/hibernate-orm-4/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-4/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -1,9 +1,10 @@
 package org.hibernate.bugs;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,31 +14,25 @@ import org.junit.Test;
  */
 public class ORMStandaloneTestCase {
 
-	private Configuration cfg;
-
 	private SessionFactory sf;
 
 	@Before
 	public void setup() {
-		cfg = new Configuration();
-
-		// Add your entities here.
-		// cfg.addAnnotatedClass(Foo.class);
-
-		// Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
-		cfg.setProperty("hibernate.show_sql", "true");
-		cfg.setProperty("hibernate.format_sql", "true");
-		cfg.setProperty("hibernate.hbm2ddl.auto", "update");
-
-		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder();
-		ServiceRegistry sr = srb.build();
-		sf = cfg.buildSessionFactory(sr);
+		Configuration config = new Configuration()
+				.configure()
+				.addInputStream( getClass().getResourceAsStream( "/Foo.hbm.xml" ) );
+		sf = config.buildSessionFactory();
 	}
 
 	// Add your tests, using standard JUnit.
 
 	@Test
 	public void hhh123Test() throws Exception {
-
+		// BaseCoreFunctionalTestCase automatically creates the SessionFactory and provides the Session.
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		s.persist( new Foo() );
+		tx.commit();
+		s.close();
 	}
 }
--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -1,9 +1,13 @@
 package org.hibernate.bugs;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,24 +21,21 @@ public class ORMStandaloneTestCase {
 
 	@Before
 	public void setup() {
-		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder()
-			// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
-			.applySetting( "hibernate.show_sql", "true" )
-			.applySetting( "hibernate.format_sql", "true" )
-			.applySetting( "hibernate.hbm2ddl.auto", "update" );
-
-		Metadata metadata = new MetadataSources( srb.build() )
-		// Add your entities here.
-		//	.addAnnotatedClass( Foo.class )
-			.buildMetadata();
-
-		sf = metadata.buildSessionFactory();
+		Configuration config = new Configuration()
+				.configure()
+				.addInputStream( getClass().getResourceAsStream( "/Foo.hbm.xml" ) );
+		sf = config.buildSessionFactory();
 	}
 
 	// Add your tests, using standard JUnit.
 
 	@Test
 	public void hhh123Test() throws Exception {
-
+		// BaseCoreFunctionalTestCase automatically creates the SessionFactory and provides the Session.
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		s.persist( new Foo() );
+		tx.commit();
+		s.close();
 	}
 }
